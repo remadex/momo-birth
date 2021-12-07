ffb11570159be17eeed7b8b5d6fbee6e50788311
Vesass <godarthupetmaxime@outlook.com>
2021-12-07 22:23:45 +0100

    ok

--- main.js
+++ main.js
@@ -29,6 +29,28 @@ $(function () {
   moveDiv();
   setInterval(moveDiv, 1000);
 
+
+  function triptyrade(){
+
+  }
+
+
+  function moveDiv1() {
+    var $span = $("#triptyrade1");
+
+    $span.fadeOut(1000, function () {
+      var maxLeft = $(window).width() - $span.width();
+      var maxTop = $(window).height() - $span.height();
+      var leftPos = Math.floor(Math.random() * (maxLeft + 1))
+      var topPos = Math.floor(Math.random() * (maxTop + 1))
+
+      $span.css({
+        left: leftPos,
+        top: topPos
+      }).fadeIn(1000);
+    });
+  };
+
   $(document).mousemove(function(e) {
       let path = ["green", "orange", "black"]
       $(".landing").css('background-color', path[Math.floor(Math.random() * path.length)]);
